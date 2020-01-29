caab4e6dd9b5c4f451c21d6f633bb07f9043ecbb
unknown <jonatasoak@gmail.com>
2020-01-29 11:04:27 -0500

    Updates

---
+++ precache-manifest.26c7302f44bb69b7b9ed087de492563f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3f420f1b016699988c295ef10a82e7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "81eaf0584abec5b45922",
+    "url": "/monsters-rolodex/static/css/main.2f619c0f.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81eaf0584abec5b45922",
+    "url": "/monsters-rolodex/static/js/main.fb0980db.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
